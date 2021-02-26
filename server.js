26a6ffff667222cdf5942d5e51421b868d858bfe
lucia <pereyra.lucia@gmail.com>
2021-02-26 20:07:27 -0300

    modifico server

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require("express") 
+const path = require('path')
+require("./config/database")
 require("dotenv").config()
 const cors = require("cors") 
 const router = require("./routes/index")
-require("./config/database")
 
 const app = express()
 
 
 
-app.use(cors()) 
 app.use(express.json()) 
+app.use(cors()) 
 
 app.use("/api", router)
 
-const port = process.env.PORT 
+const port = process.env.PORT || 4000 
 const host = process.env.HOST || '0.0.0.0'
 
 
-
-app.listen(port,host, () => console.log("App listening on port 4000")) 
+app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}`))
+// app.listen(port,host, () => console.log("App listening on port 4000")) 
